bd591f950a9528fbfd7f54467b2f9c8a0b72fdd2
jaister <jaljalico@gmail.com>
2024-04-08 01:10:07 +0200

    Contenedor funcional, listo para clonar en VM

--- Psoft/Psoft/settings.py
+++ Psoft/Psoft/settings.py
@@ -28,7 +28,7 @@ AUTHENTICATION_BACKENDS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,

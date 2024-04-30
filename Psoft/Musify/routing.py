864575c5e9b55b51fb21cd04baad6396ba9df2ae
jaister <jaljalico@gmail.com>
2024-04-30 18:27:44 +0200

    Arreglos chat

--- Psoft/Musify/routing.py
+++ Psoft/Musify/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', views.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
